68aa564a6fec3580ea97be692abcb41c8e647ef6
Whitney <whitzhu@gmail.com>
2019-07-28 23:23:15 -0700

    Moved App into  own component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React Boiler Plate 2019</h1>
+        <p>Typescript</p>
+        <p>Webpack-dev-server</p>
+      </div>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>React Boiler Plate 2019</h1>
-        <p>Typescript</p>
-        <p>Webpack-dev-server</p>
-      </div>
-    );
-  }
-}
+import App from "./components/App/App.tsx";
 
 ReactDOM.render(<App />, document.getElementById("root"));
